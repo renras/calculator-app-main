e47677fbc1787e2b5f784771809c1d57e34dd730
Renzo Visperas <renzovisperas07@gmail.com>
2022-01-14 23:15:03 +0800

    add theme functionality

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
     }
 
     if (e.target.getAttribute("id") === "key-equals") {
-      $("#screen-text").text(eval($("#screen-text").text()));
+      $("#screen-text").text(eval($("#screen-text").text().replace("x", "*")));
     }
 
     if (e.target.getAttribute("id") === "key-reset") {
@@ -104,15 +104,192 @@ $(document).ready(function () {
 
   $("#theme-number").click((e) => {
     if (e.target.getAttribute("id") === "theme-number-1") {
+      $("html").fadeOut(0);
+      $("html").fadeIn(500);
+
       changeCurrentActiveThemeNumber(1, "6px");
+
+      $("html").css("background-color", "#3b4664");
+
+      $("#calculator").css("background-color", "#3b4664");
+
+      $("h1").css("color", "#fdfffe");
+
+      $("h2").css("color", "#fdfffe");
+
+      $("li").css("color", "#fdfffe");
+
+      $("#pill").css("background-color", "#252d45");
+
+      $("#circle").css("background-color", "#cb3e30");
+
+      $("#screen").css("background-color", "#181f32");
+
+      $("#screen-text").css("color", "#fdfffe");
+
+      $("#keypad").css("background-color", "#252d44");
+
+      $(".number-key").css({
+        "background-color": "#eae3db",
+        "box-shadow": "0px 3px 0px #b4a397",
+        color: "#404354",
+      });
+
+      $(".operator-key").css({
+        "background-color": "#eae3db",
+        "box-shadow": "0px 3px 0px #b4a397",
+        color: "#404354",
+      });
+
+      $("#key-decimal").css({
+        "background-color": "#eae3db",
+        "box-shadow": "0px 3px 0px #b4a397",
+        color: "#404354",
+      });
+
+      $("#key-del").css({
+        "background-color": "#647299",
+        "box-shadow": "0px 3px 0px #414e72",
+        color: "#fcffff",
+      });
+
+      $("#key-reset").css({
+        "background-color": "#647299",
+        "box-shadow": "0px 3px 0px #414e72",
+        color: "#fcffff",
+      });
+
+      $("#key-equals").css({
+        "background-color": "#d13f30",
+        "box-shadow": "0px 3px 0px #762a28",
+        color: "#fff5f0",
+      });
     }
 
     if (e.target.getAttribute("id") === "theme-number-2") {
+      $("html").fadeOut(0);
+      $("html").fadeIn(500);
+
       changeCurrentActiveThemeNumber(2, "38%");
+
+      $("html").css("background-color", "#e6e6e6");
+
+      $("#calculator").css("background-color", "#e6e6e6");
+
+      $("h1").css("color", "#3b3b31");
+
+      $("h2").css("color", "#3b3b31");
+
+      $("li").css("color", "#3b3b31");
+
+      $("#pill").css("background-color", "#d3cdcd");
+
+      $("#circle").css("background-color", "#c85a0a");
+
+      $("#screen").css("background-color", "#eeeeee");
+
+      $("#screen-text").css("color", "#3b3b31");
+
+      $("#keypad").css("background-color", "#d3cdcd");
+
+      $(".number-key").css({
+        "background-color": "#e5e4e0",
+        "box-shadow": "0px 3px 0px #a69d91",
+        color: "#33332a",
+      });
+
+      $(".operator-key").css({
+        "background-color": "#e5e4e0",
+        "box-shadow": "0px 3px 0px #a69d91",
+        color: "#33332a",
+      });
+
+      $("#key-decimal").css({
+        "background-color": "#e5e4e0",
+        "box-shadow": "0px 3px 0px #a69d91",
+        color: "#33332a",
+      });
+
+      $("#key-del").css({
+        "background-color": "#388187",
+        "box-shadow": "0px 3px 0px #1b5f66",
+        color: "#f3ffff",
+      });
+
+      $("#key-reset").css({
+        "background-color": "#388187",
+        "box-shadow": "0px 3px 0px #1b5f66",
+        color: "#f3ffff",
+      });
+
+      $("#key-equals").css({
+        "background-color": "#c85401",
+        "box-shadow": "0px 3px 0px #83491f",
+        color: "#f3ffff",
+      });
     }
 
     if (e.target.getAttribute("id") === "theme-number-3") {
+      $("html").fadeOut(0);
+      $("html").fadeIn(500);
+
       changeCurrentActiveThemeNumber(3, "67%");
+
+      $("html").css("background-color", "#17062a");
+
+      $("#calculator").css("background-color", "#17062a");
+
+      $("h1").css("color", "#fde140");
+
+      $("h2").css("color", "#fde140");
+
+      $("li").css("color", "#fde140");
+
+      $("#pill").css("background-color", "#1e0837");
+
+      $("#circle").css("background-color", "#00dace");
+
+      $("#screen").css("background-color", "#1e0836");
+
+      $("#screen-text").css("color", "#fde140");
+
+      $("#keypad").css("background-color", "#1e0836");
+
+      $(".number-key").css({
+        "background-color": "#331b4d",
+        "box-shadow": "0px 3px 0px #7c219a",
+        color: "#fde140",
+      });
+
+      $(".operator-key").css({
+        "background-color": "#331b4d",
+        "box-shadow": "0px 3px 0px #7c219a",
+        color: "#fde140",
+      });
+
+      $("#key-decimal").css({
+        "background-color": "#331b4d",
+        "box-shadow": "0px 3px 0px #7c219a",
+        color: "#fde140",
+      });
+
+      $("#key-del").css({
+        "background-color": "#5a057c",
+        "box-shadow": "0px 3px 0px #be16f4",
+        color: "#fde140",
+      });
+
+      $("#key-reset").css({
+        "background-color": "#5a057c",
+        "box-shadow": "0px 3px 0px #be16f4",
+        color: "#fde140",
+      });
+
+      $("#key-equals").css({
+        "background-color": "#00decf",
+        "box-shadow": "0px 3px 0px #74f4ef",
+        color: "#002d34",
+      });
     }
   });
 });
